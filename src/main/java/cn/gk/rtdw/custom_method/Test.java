913cef3fb493a504818cd0cb696197a9864014c7
JiangHt <JiangHt@123.com>
2022-06-30 11:32:00 +0800

    国开在线-实时项目 v1

---
+++ src/main/java/cn/gk/rtdw/custom_method/Test.java
@@ -0,0 +1,9 @@
+package cn.gk.rtdw.custom_method;
+
+/**
+ * @Date：2022/6/30
+ * @Description： 自定义udf
+ */
+public class Test {
+
+}
---
+++ src/main/java/cn/gk/rtdw/pojo/Pojo.java
@@ -0,0 +1,8 @@
+package cn.gk.rtdw.pojo;
+
+/**
+ * @Date：2022/6/30
+ * @Description： 自定义 bean类
+ */
+public class Pojo {
+}
