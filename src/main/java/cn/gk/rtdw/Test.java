e35350b57eda09c46eef36061f14f0ca59519a8c
17602391150 <916866820Li>
2022-06-30 10:21:34 +0800

    testt

--- src/main/java/cn/gk/rtdw/Test.java
+++ src/main/java/cn/gk/rtdw/Test.java
@@ -10,5 +10,7 @@ public class Test {
         System.out.println("hello 海通");
         System.out.println("hello 清懿");
         System.out.println("hello 海涛");
+        System.out.println("hello 海涛");
+        System.out.println("hello 海涛");
     }
 }
