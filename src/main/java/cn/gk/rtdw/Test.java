581f4640b9284185423438b16d863e91ad53e6b0
JiangHt <JiangHt@123.com>
2022-06-30 09:50:32 +0800

    国开在线-实时项目 v1

---
+++ src/main/java/cn/gk/rtdw/Test.java
@@ -0,0 +1,13 @@
+package cn.gk.rtdw;
+
+/**
+ * @Author：HT
+ * @Date：2022/6/30
+ * @Description： 测试类
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello 海通");
+        System.out.println("hello 清懿");
+    }
+}
